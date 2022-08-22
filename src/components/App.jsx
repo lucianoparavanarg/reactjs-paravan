82ed909afae1acdd0c4af76f69e3613a69f266d0
lucianoparavanarg <lucianoparavanarg@gmail.com>
2022-08-22 00:40:55 -0300

    cambios en navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="">
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">Tienda de bebidas</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
