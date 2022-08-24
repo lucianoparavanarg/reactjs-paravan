a4ca955a793a12be4cb172a9680ba3718124e05e
lucianoparavanarg <lucianoparavanarg@gmail.com>
2022-08-24 07:26:21 -0300

    componentes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,10 @@
 import './App.css';
+import NavBar from './NavBar';
 
+NavBar
 function App() {
   return (
-    <div className="">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Tienda de bebidas</a>
-          <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">PRODUCTOS</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">NOSOTROS</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">CONTACTO</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-    </div>
+    <NavBar/>
   );
 }
 
