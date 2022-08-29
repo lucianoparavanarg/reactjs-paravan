d2636b753182a776a42146e8d93b388766ef659a
lucianoparavanarg <lucianoparavanarg@gmail.com>
2022-08-29 19:18:10 -0300

    componentes contador con boton

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,40 @@
+import {useState} from 'react'
+
+export const ItemCount = ({stock, onAdd}) => {
+    const [count, setCount] = useState(0)
+
+    function add(){
+      if(count < stock){
+      setCount(count + 1)
+    }
+    }
+
+    function substract(){
+      if (count > 0){
+      setCount(count - 1)
+    }
+    }
+
+    function reset(){
+      setCount(0)
+    }
+
+  return (
+    <div>
+        <div className='counter-box'>
+            <h3 className='my-2'>Counter</h3>
+            <p>Stock: {stock}</p>
+            <p>Cantidad: {count}</p>
+
+            <div>
+                <button className='btn btn-outline-danger btn-m' onClick={substract}> - </button>
+                <button className='btn btn-outline-primary btn-m' onClick={add}> + </button>
+                <button className='btn btn-outline-warning btn-m' onClick={reset}> Reset</button>
+            </div>
+            <div>
+              <button className='btn btn-primary btn-m' onClick={() => onAdd(count)}>Confirmar</button>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { ItemCount } from '../ItemCount/ItemCount'
 
 export const ItemListContainer = () => {
+  function onAdd(count){
+    console.log(`Se seleccionaron ${count} productos`)
+  }
+
   return (
-    <h1>Nuestros Productos</h1>
+    <div>
+      <h1 className='my-5'>Nuestros Productos</h1>
+      <ItemCount stock={8} onAdd={onAdd}/>
+    </div>
   )
 }
