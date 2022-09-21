09f43db2d3debee8e5f21fc640a1373698a42b2a
lucianoparavanarg <lucianoparavanarg@gmail.com>
2022-09-21 19:20:39 -0300

    errores useEffect

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -13,10 +13,13 @@ export const ItemListContainer = () => {
   
 
   useEffect(()=> {
-    if (categoria) {
       fetch("./json/productos.json")
       .then(response => response.json())
       .then(data => {
+        if(categoria){
+          productCard = data.filter(products => products.categoria === categoria)
+          
+        } else {
         const productCard = data.map((products, indice)=>
         <div className="card cardListStyle" key={indice} >
             <img src={"./img/" + products.img} className="card-img-top" alt="..." style={{width:"auto", height:"200px"}}/>
@@ -29,33 +32,12 @@ export const ItemListContainer = () => {
                 <button className='btn btn-dark'><Link className='nav-link' to={"/productos/" + products.id}>Ver Productos</Link></button>
               </div>
         </div>
+        }
         )
         setProducts(productCard)
       })
       .catch(error => console.error(error))
     }, [categoria]);
-    } else {
-      fetch("./json/productos.json")
-      .then(response => response.json())
-      .then(data => {
-        const productCard = data.map((products, indice)=>
-        <div className="card cardListStyle" key={indice} >
-            <img src={"./img/" + products.img} className="card-img-top" alt="..." style={{width:"auto", height:"200px"}}/>
-              <div className="card-body">
-                <h5 className="card-title">{products.nombre}</h5>
-                <p className="card-text">Marca: {products.marca}</p>
-                <p className="card-text">Modelo: {products.modelo}</p>
-                <p className="card-text">Precio: ${products.precio}</p>
-                <p className="card-text">Stock: {products.stock}</p>
-                <button className='btn btn-dark'><Link className='nav-link' to={"/productos/" + products.id}>Ver Productos</Link></button>
-              </div>
-        </div>
-        )
-        setProducts(productCard)
-      })
-      .catch(error => console.error(error))
-    }, []);
-
     
 
   return (
